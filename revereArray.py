53d416a6134d6f0df583166aaaf5747d9a67cce0
Abhinay Rai <42450450+Surajraikaushik@users.noreply.github.com>
2020-04-27 21:00:22 -0700

    Update and rename PRO.py to revereArray.py

---
+++ revereArray.py
@@ -0,0 +1,12 @@
+def reverseArray(array , rev_order):
+  ws =[]
+  f = len(sd)
+  for i in range(rev_order , f):
+    ws.append(array[i])
+  for j in range(rev_order):
+    d = len(ws)
+    ws.insert(d, array[j])
+    
+   print(ws)
+ sd= [1,2,3,4,5,6,7,8]
+reverseArray(sd,3)
