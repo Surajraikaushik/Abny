eae856698e211919a414e4f9d609d1a1cc0868c4
Abhinay Rai <42450450+Surajraikaushik@users.noreply.github.com>
2020-04-27 22:42:27 -0700

    Add files via upload

---
+++ .github/Average App.py
@@ -0,0 +1,17 @@
+import math
+print("Welcome to Average App")
+
+name = str(input("Enter the Student name: "))
+
+no_grades = int(input("Num of grades u want to enter: "))
+
+grades = []
+for i in range(no_grades):
+    grade = float(input("Enter  " +str(i+1) + "st grade "))
+    grades.append(grade)
+sd= sum(grades)/len(grades)
+
+s =round(sd ,3)
+print(grades)
+                  
+print("\n" + str(s))
